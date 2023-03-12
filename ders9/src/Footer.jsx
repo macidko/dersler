734b9145c064d2ce3bd448cc46bc4c4c8376450a
Eren Macit <ernmctt@gmail.com>
2023-03-12 19:44:04 +0300

    feat: genel UI oluşturuldu

---
+++ ders9/src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(ozellikler) {
+  const icerik = "Gelistirici bilgileri";
+
+  console.log("Ben de footer...");
+
+  return (
+    <footer>
+      <p>{icerik}</p>
+    </footer>
+  );
+}
+
+export default Footer;
